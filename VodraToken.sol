885f66262d9be9e70cc736bd3961f8c680c67e08
ZacharyBys <zachary.bys@gmail.com>
2021-02-02 21:55:02 -0500

    Remove doc string comments from ERC20 interface

--- VodraToken.sol
+++ VodraToken.sol
@@ -5,9 +5,9 @@ import "./StandardToken.sol";
 
 contract VodraToken is StandardToken {
 
+    uint8 public constant decimals = 18;
     string public constant name = "Vodra";
     string public constant symbol = "VDR";
-    uint8 public constant decimals = 18;
     uint256 public constant totalTokens = 2 * (10**9) * 10**decimals;
     bytes32 public version = bytes32("1.0");
 
