df9be6c12b2191e32c10502d9aa44981dd5434bb
ZacharyBys <zachary.bys@gmail.com>
2021-02-02 22:12:23 -0500

    Optimize metadata variables for gas fees

--- VodraToken.sol
+++ VodraToken.sol
@@ -6,10 +6,10 @@ import "./StandardToken.sol";
 contract VodraToken is StandardToken {
 
     uint8 public constant decimals = 18;
-    string public constant name = "Vodra";
-    string public constant symbol = "VDR";
-    uint256 public constant totalTokens = 2 * (10**9) * 10**decimals;
+    bytes32 public constant name = bytes32("Vodra");
+    bytes32 public constant symbol = bytes32("VDR");
     bytes32 public version = bytes32("1.0");
+    uint256 public constant totalTokens = 2 * (10**9) * 10**decimals;
 
     constructor() {
         totalSupply_ = totalTokens;
