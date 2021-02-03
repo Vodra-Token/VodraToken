de242316601429541e20baab0f55ff5e680e1567
ZacharyBys <zachary.bys@gmail.com>
2021-02-02 23:16:07 -0500

    Add shell for redemption and multiple transfer functions

--- VodraToken.sol
+++ VodraToken.sol
@@ -15,4 +15,20 @@ contract VodraToken is StandardToken {
         totalSupply_ = totalTokens;
         balances[msg.sender] = totalSupply_;
     }
+
+    function transferMultiple(address _from, address[] _toArray, uint256[] _valueArray) {
+        require(_toArray.length == _valueArray.length);
+
+        for (uint i=0; i< _toArray.length; i++) {
+            // Transfers from _from to current value in _toArray
+        }
+    }
+
+    function transferFromMultiple(address _to, address[] _fromArray, uint256[] _valueArray) {
+        require(_fromArray.length == _valueArray.length);
+
+        for (uint i=0; i< _fromArray.length; i++) {
+            // Transfers from current value in _fromArray to _to
+        }
+    }
 }
