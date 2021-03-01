613ef55ffcb6043102f8b2c122c34bf3a93eacea
ZacharyBys <zachary.bys@gmail.com>
2021-02-28 20:09:16 -0500

    Update creator redemptions function

--- VodraToken.sol
+++ VodraToken.sol
@@ -16,35 +16,14 @@ contract VodraToken is StandardToken {
         balances[msg.sender] = totalSupply_;
     }
 
-    function creatorRedemptions(address[] memory _redemptionRecipients, uint[] memory _values) public returns (bool) {
+    function creatorRedemptions(address[] memory _redemptionRecipients, uint256[] memory _values) public returns (bool) {
         require(_redemptionRecipients.length == _values.length);
 
-        uint256 senderBalance = balances[msg.sender];
         uint256 length = _redemptionRecipients.length;
         for (uint i = 0; i < length; i++) {
-            uint value = _values[i];
-            address recipient = _redemptionRecipients[i];
-
-            require(senderBalance >= value);
-            if (msg.sender != _redemptionRecipients[i]) {
-                senderBalance -= value;
-                balances[recipient] += value;
-            }
-
-            emit Transfer(msg.sender, recipient, value);
+            transfer(msg.sender, _values[i]);
         }
 
-        balances[msg.sender] = senderBalance;
         return true;
     }
-
-    // Will require approval functions from individual senders
-    // function transferFromMultiple(address _to, address[] _fromArray, uint256[] _valueArray) public returns (bool) {
-    //     require(_fromArray.length == _valueArray.length);
-
-    //     uint256 length = _fromArray.length;
-    //     for (uint i=0; i< length; i+=1) {
-    //         // Transfers from current value in _fromArray to _to
-    //     }
-    // }
 }
